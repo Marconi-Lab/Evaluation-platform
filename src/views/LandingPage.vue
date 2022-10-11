62e13c28f03716edd5a13e88208333592d28cd06
NakacwaOlivia <k.nakacwa.olivia@gmail.com>
2022-10-11 14:44:45 +0300

    Added navbar

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,7 +1,10 @@
 <template>
-  <nav class="navbar bg-light">
-    <div class="container-fluid" style="background-color: #3b8ea5">
+  <nav class="navbar" style="background-color: #3b8ea5">
+    <div class="container-fluid">
       <a class="navbar-brand ms-auto text-white" href="#">Navbar</a>
     </div>
   </nav>
+  <div>
+    <h4 class="text-dark mt-3">The Marconi Lab ML Evaluation Platform</h4>
+  </div>
 </template>
