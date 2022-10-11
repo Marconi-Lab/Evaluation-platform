2f96d3dbf108fc05b88db8b713d4ffa0d06e0149
NakacwaOlivia <k.nakacwa.olivia@gmail.com>
2022-10-11 15:36:19 +0300

    added cards for the images

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,10 +1,105 @@
 <template>
-  <nav class="navbar" style="background-color: #3b8ea5">
-    <div class="container-fluid">
+  <nav class="navbar navbar-expand-lg" style="background: #3891a6">
+    <div class="container-fluid col-12">
       <a class="navbar-brand ms-auto text-white" href="#">Navbar</a>
     </div>
   </nav>
   <div>
     <h4 class="text-dark mt-3">The Marconi Lab ML Evaluation Platform</h4>
+  </div>
+  <div class="row row-cols-1 row-cols-md-4 g-4 mt-5">
+    <div class="col">
+      <div class="card h-100 ms-2 me-2">
+        <img src="" class="card-img-top" alt="" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100 ms-2 me-2">
+        <img src="" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a short card.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100 ms-2 me-2">
+        <img src="" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100 ms-2 me-2">
+        <img src="" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100 ms-2 me-2">
+        <img src="" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100 ms-2 me-2">
+        <img src="" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100 ms-2 me-2">
+        <img src="" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100 ms-2 me-2">
+        <img src="" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
