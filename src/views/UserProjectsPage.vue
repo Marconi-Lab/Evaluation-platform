ea6917c19ee357ba455452b40f5d1f43f9d326ea
ben-wycliff <ben12wycliff@gmail.com>
2022-10-12 22:03:57 +0300

    Removes multiple static cards

--- src/views/UserProjectsPage.vue
+++ src/views/UserProjectsPage.vue
@@ -4,104 +4,25 @@
       <a class="navbar-brand ms-auto text-white" href="#">Navbar</a>
     </div>
   </nav>
-  <div>
-    <h4 class="text-dark mt-3">The Marconi Lab ML Evaluation Platform</h4>
-  </div>
-  <div class="row row-cols-1 row-cols-md-4 g-4 mt-5">
-    <div class="col">
-      <div class="card h-100 ms-2 me-2">
-        <img
-          src="../assets/images/A-cassava-plant-affected-by-mosaic-wilt.-Source-Flickr.webp"
-          class="card-img-top"
-          alt=""
-        />
-        <div class="card-body me-2">
-          <p class="card-text">
-            <a href="#" class="btn text-primary"
-              >Detecting diseases in a cassava crop</a
-            >
-          </p>
-        </div>
-      </div>
+  <div class="container">
+    <div>
+      <h4 class="text-dark mt-3">The Marconi Lab ML Evaluation Platform</h4>
     </div>
-    <div class="col">
-      <div class="card h-100 ms-2 me-2">
-        <img src="" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a short card.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card h-100 ms-2 me-2">
-        <img :src="img2" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card h-100 ms-2 me-2">
-        <img :src="img3" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card h-100 ms-2 me-2">
-        <img :src="img4" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card h-100 ms-2 me-2">
-        <img :src="img5" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card h-100 ms-2 me-2">
-        <img :src="img6" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card h-100 ms-2 me-2">
-        <img :src="img7" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </p>
+    <div class="row row-cols-1 row-cols-md-4 g-4 mt-5">
+      <div class="col">
+        <div class="card h-100 ms-2 me-2">
+          <img
+            src="../assets/images/A-cassava-plant-affected-by-mosaic-wilt.-Source-Flickr.webp"
+            class="card-img-top"
+            alt=""
+          />
+          <div class="card-body me-2">
+            <p class="card-text">
+              <a href="#" class="btn text-primary"
+                >Detecting diseases in a cassava crop</a
+              >
+            </p>
+          </div>
         </div>
       </div>
     </div>
