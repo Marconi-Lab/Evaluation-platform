564f4ffec33b2abe28b0e4c9730aba9a070a9719
ben-wycliff <ben12wycliff@gmail.com>
2022-10-12 21:19:25 +0300

    Replaces form with form component

--- src/views/LoginScreen.vue
+++ src/views/LoginScreen.vue
@@ -11,48 +11,7 @@
         </div>
       </div>
       <div class="col-sm-6 d-flex justify-content-center align-items-center">
-        <form style="background-color: rgb(215, 215, 215)">
-          <div class="mb-3 text-start">
-            <label
-              for="validationCustom01"
-              class="form-label text-dark mt-5 ms-5 rounded-4"
-              >Team name</label
-            >
-            <input
-              type="text"
-              class="form-control ms-5 me-2"
-              id="validationCustom01"
-              required
-            />
-          </div>
-          <div class="mb-4 text-start">
-            <label for="exampleInputPassword1" class="form-label text-dark ms-5"
-              >Password</label
-            >
-            <input
-              v-model="password"
-              :rules="passwordRules"
-              :type="passwordShow ? 'text' : 'password'"
-              prepend-inner-icon="mdi-key"
-              :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="passwordShow = !passwordShow"
-              required
-              class="form-control ms-5"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <router-link to="/landingpage" class="text-white"
-            ><button
-              type="submit"
-              class="btn btn-primary mb-5 ms-1"
-              style="background-color: #3891a6"
-              @click="doLogin"
-            >
-              Login
-              <!-- <RouterLink h></RouterLink><a href="/#/landingpage" class="text-white">Login</a> -->
-            </button></router-link
-          >
-        </form>
+        <auth-form />
       </div>
     </div>
   </div>
@@ -60,19 +19,13 @@
 
 <script>
 import LandingNavigation from "@/components/common/LandingNavigation.vue";
+import AuthForm from "@/components/common/AuthForm.vue";
 export default {
   name: "login-screen",
   components: {
     LandingNavigation,
+    AuthForm,
   },
-  data: () => ({
-    passwordShow: false,
-    password: "",
-    passwordRules: [
-      (v) => !!v || "Password is required",
-      (v) => (v && v.length >= 6) || "Password must be 6  characters or more!",
-    ],
-  }),
 };
 </script>
 
