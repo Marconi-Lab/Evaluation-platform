042ae85ddbbade93954388da0bd5e2ab0c1bea2d
NakacwaOlivia <k.nakacwa.olivia@gmail.com>
2022-10-12 15:32:43 +0300

    removes default navigation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/signup">Signup</router-link> |
-    <router-link to="/landingpage">Landing Page</router-link>
-  </nav>
   <router-view />
 </template>
 
