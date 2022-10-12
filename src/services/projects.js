af1fe0a3e29236713cae9290e4e1ba6890c757aa
ben-wycliff <ben12wycliff@gmail.com>
2022-10-12 21:53:30 +0300

    Creates a projects dummy service

---
+++ src/services/projects.js
@@ -0,0 +1,115 @@
+module.exports = [
+  {
+    id: 1,
+    image_url:
+      "https://d3i71xaburhd42.cloudfront.net/1afe2e2397e3ff74e488b52f693519f669332cb2/34-Figure2.3-1.png",
+    title: "Detecting diseases in the cassava crop",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Nulla pharetra mi eget ornare venenatis. Quisque placerat sit amet dui id convallis. \
+    Maecenas imperdiet eget orci ac posuere. Cras eget diam accumsan arcu mattis vestibulum\
+    vitae ac orci. Maecenas a accumsan erat. Nullam aliquam sed felis non varius. Nulla\
+    viverra risus id vulputate facilisis. Phasellus sit amet quam non sapien sodales\
+    viverra vitae sed tortor. Cras vehicula finibus odio non ornare. Phasellus dignissim,\
+    ipsum auctor porta ultrices, justo mauris vehicula ligula, eget rutrum eros risus vel diam.",
+  },
+  {
+    id: 2,
+    image_url:
+      "https://storage.googleapis.com/cgiarorg/2020/05/636a705d-whiteflies.jpg",
+    title: "Estimating the number of whiteflies on a cassava leaf",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Nulla pharetra mi eget ornare venenatis. Quisque placerat sit amet dui id convallis. \
+    Maecenas imperdiet eget orci ac posuere. Cras eget diam accumsan arcu mattis vestibulum\
+    vitae ac orci. Maecenas a accumsan erat. Nullam aliquam sed felis non varius. Nulla\
+    viverra risus id vulputate facilisis. Phasellus sit amet quam non sapien sodales\
+    viverra vitae sed tortor. Cras vehicula finibus odio non ornare. Phasellus dignissim,\
+    ipsum auctor porta ultrices, justo mauris vehicula ligula, eget rutrum eros risus vel diam.",
+  },
+  {
+    id: 3,
+    image_url:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.growveg.com%2Fplant-diseases%2Fus-and-canada%2Fbean-rust%2F&psig=AOvVaw2t-50skma9U8fTaO8jH3B6&ust=1665686713832000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLC47tqs2_oCFQAAAAAdAAAAABAE",
+    title: "Detecting diseases in the bean crop",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Nulla pharetra mi eget ornare venenatis. Quisque placerat sit amet dui id convallis. \
+    Maecenas imperdiet eget orci ac posuere. Cras eget diam accumsan arcu mattis vestibulum\
+    vitae ac orci. Maecenas a accumsan erat. Nullam aliquam sed felis non varius. Nulla\
+    viverra risus id vulputate facilisis. Phasellus sit amet quam non sapien sodales\
+    viverra vitae sed tortor. Cras vehicula finibus odio non ornare. Phasellus dignissim,\
+    ipsum auctor porta ultrices, justo mauris vehicula ligula, eget rutrum eros risus vel diam.",
+  },
+  {
+    id: 4,
+    image_url:
+      "https://live.staticflickr.com/7481/15806177742_670ff34930_b.jpg",
+    title: "Detecting diseases in the passion fruit disease",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Nulla pharetra mi eget ornare venenatis. Quisque placerat sit amet dui id convallis. \
+    Maecenas imperdiet eget orci ac posuere. Cras eget diam accumsan arcu mattis vestibulum\
+    vitae ac orci. Maecenas a accumsan erat. Nullam aliquam sed felis non varius. Nulla\
+    viverra risus id vulputate facilisis. Phasellus sit amet quam non sapien sodales\
+    viverra vitae sed tortor. Cras vehicula finibus odio non ornare. Phasellus dignissim,\
+    ipsum auctor porta ultrices, justo mauris vehicula ligula, eget rutrum eros risus vel diam.",
+  },
+  {
+    id: 5,
+    image_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7pE_De_9-aRQdk6dc4TOxe25A3uD308THKw&usqp=CAU",
+    title: "Automatic classification of lung diseases using ultrasound images",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Nulla pharetra mi eget ornare venenatis. Quisque placerat sit amet dui id convallis. \
+    Maecenas imperdiet eget orci ac posuere. Cras eget diam accumsan arcu mattis vestibulum\
+    vitae ac orci. Maecenas a accumsan erat. Nullam aliquam sed felis non varius. Nulla\
+    viverra risus id vulputate facilisis. Phasellus sit amet quam non sapien sodales\
+    viverra vitae sed tortor. Cras vehicula finibus odio non ornare. Phasellus dignissim,\
+    ipsum auctor porta ultrices, justo mauris vehicula ligula, eget rutrum eros risus vel diam.",
+  },
+  {
+    id: 6,
+    image_url:
+      "https://screening.iarc.fr/viavilipici/5.1.2-cervical%20precancerjpg.jpg",
+    title: "Automatic diagnosis of cervical cancer from colposcopic images",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Nulla pharetra mi eget ornare venenatis. Quisque placerat sit amet dui id convallis. \
+    Maecenas imperdiet eget orci ac posuere. Cras eget diam accumsan arcu mattis vestibulum\
+    vitae ac orci. Maecenas a accumsan erat. Nullam aliquam sed felis non varius. Nulla\
+    viverra risus id vulputate facilisis. Phasellus sit amet quam non sapien sodales\
+    viverra vitae sed tortor. Cras vehicula finibus odio non ornare. Phasellus dignissim,\
+    ipsum auctor porta ultrices, justo mauris vehicula ligula, eget rutrum eros risus vel diam.",
+  },
+  {
+    id: 7,
+    image_url:
+      "https://i0.wp.com/bdtechtalks.com/wp-content/uploads/2022/06/large-language-model-logical-reasoning.jpg?ssl=1",
+    title: "Building a kiswahili language model",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Nulla pharetra mi eget ornare venenatis. Quisque placerat sit amet dui id convallis. \
+    Maecenas imperdiet eget orci ac posuere. Cras eget diam accumsan arcu mattis vestibulum\
+    vitae ac orci. Maecenas a accumsan erat. Nullam aliquam sed felis non varius. Nulla\
+    viverra risus id vulputate facilisis. Phasellus sit amet quam non sapien sodales\
+    viverra vitae sed tortor. Cras vehicula finibus odio non ornare. Phasellus dignissim,\
+    ipsum auctor porta ultrices, justo mauris vehicula ligula, eget rutrum eros risus vel diam.",
+  },
+  {
+    id: 8,
+    image_url:
+      "https://d33wubrfki0l68.cloudfront.net/9e1b2a906ae6b01cfe2d5d237e1e51f5d41864e3/2a5f9/static/348bb1d70089176ca2f61ea402094382/50bf7/main.png",
+    title:
+      "Sentiment analysis on reviews from customers to an online shopping platform.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Nulla pharetra mi eget ornare venenatis. Quisque placerat sit amet dui id convallis. \
+    Maecenas imperdiet eget orci ac posuere. Cras eget diam accumsan arcu mattis vestibulum\
+    vitae ac orci. Maecenas a accumsan erat. Nullam aliquam sed felis non varius. Nulla\
+    viverra risus id vulputate facilisis. Phasellus sit amet quam non sapien sodales\
+    viverra vitae sed tortor. Cras vehicula finibus odio non ornare. Phasellus dignissim,\
+    ipsum auctor porta ultrices, justo mauris vehicula ligula, eget rutrum eros risus vel diam.",
+  },
+];
