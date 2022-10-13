a88d26b3ee86e9403091988cc089301d5c4a1456
ben-wycliff <ben12wycliff@gmail.com>
2022-10-13 19:57:54 +0300

    Adds To-Do

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,10 +9,17 @@ router.post("/register", auth.signup);
 
 router.post("/login", auth.login);
 
+// labels evaluation should be done at the time of upload
 router.post("/file", up.upload);
 
 router.get("/projects", proj.findProject);
 
+// the controller in this endpoint should be embedded in the file upload endpoint
+// thus there is no need for this
 router.get("/score", scores.score);
+
+// TODO - DELETE /project/<projectID> (admin only)
+// TODO - GET /project/<projectID>
+// TODO - GET /evaluation/<projectID> (returns evaluation in descending order of prediction scores)
 
 module.exports = router;
