34aa8b827303cf8880e21743d3497adb321e8cc9
ben-wycliff <ben12wycliff@gmail.com>
2022-10-15 10:18:54 +0300

    Adds search component

--- frontend/src/views/admin/Projects.vue
+++ frontend/src/views/admin/Projects.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
     <UserNavigation :username="'Admin'" />
+    <div class="container">
+      <div class="my-4">
+        <h4>The Marconi Lab ML Evaluation Platform</h4>
+        <SearchBar />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import UserNavigation from "@/components/common/UserNavigation.vue";
+import SearchBar from "@/components/common/SearchBar.vue";
 export default {
   name: "admin-projects",
   components: {
     UserNavigation,
+    SearchBar,
   },
   data() {
     return {};
