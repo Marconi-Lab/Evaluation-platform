8eba8c16860af30d1139d0698bb8ae2dee2897cf
ben-wycliff <ben12wycliff@gmail.com>
2022-10-15 11:18:28 +0300

    Creates projects table component

---
+++ frontend/src/components/admin/ProjectsTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Project</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in data" :key="row.id">
+        <td>{{ row.title }}</td>
+        <td>
+          <span
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+              /></svg
+          ></span>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import projects from "@/services/projects";
+export default {
+  name: "projects-table",
+
+  data() {
+    return {
+      data: [],
+    };
+  },
+  methods: {
+    async getProjects() {
+      this.data = projects;
+      console.log(this.data);
+    },
+  },
+  created() {
+    this.getProjects();
+  },
+};
+</script>
+
+<style></style>
