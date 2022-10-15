ccd41b6cce91ac65dad4d8ee8bc3f229a1ab8f31
bruceMug <102650525+bruceMug@users.noreply.github.com>
2022-10-15 12:17:27 +0300

    completed some TODOs

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../controllers/authController");
-const up = require("../controllers/uploadContoller");
-const scores = require("../controllers/comparisonController");
+const auth = require("../controllers/auth");
+const file = require("../controllers/upload");
+const scores = require("../controllers/comparison");
 const proj = require("../utils/");
 
 router.post("/register", auth.signup);
@@ -10,16 +10,25 @@ router.post("/register", auth.signup);
 router.post("/login", auth.login);
 
 // labels evaluation should be done at the time of upload
-router.post("/file", up.upload);
+router.post("/file", file.upload);
 
-router.get("/projects", proj.findProject);
+router.get("/projects", proj.getProjects);
 
 // the controller in this endpoint should be embedded in the file upload endpoint
 // thus there is no need for this
 router.get("/score", scores.score);
 
+
+// router.put('/addproject', auth.isAdmin, proj.putProject);
+
 // TODO - DELETE /project/<projectID> (admin only)
+router.delete("/project/:project_id", auth.isAdmin, proj.deleteProject);
+
 // TODO - GET /project/<projectID>
+router.get("/project/:project_id", proj.getProject);
+
 // TODO - GET /evaluation/<projectID> (returns evaluation in descending order of prediction scores)
+// check the table for the project id 
+router.get("/evaluation/:project_id", proj.findEval, )
 
 module.exports = router;
