a0449e27d04c7b059f04b0fa4b2a99e4a89e66e7
ben-wycliff <ben12wycliff@gmail.com>
2022-10-15 11:54:47 +0300

    Creates primary button component

---
+++ frontend/src/components/common/PrimaryButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="btn my-btn" @click="handleButtonClick">Add Project</button>
+</template>
+
+<script>
+export default {
+  name: "primary-button",
+  methods: {
+    handleButtonClick() {
+      this.$emit("onClick");
+    },
+  },
+};
+</script>
+
+<style>
+.my-btn {
+  background-color: #3891a6;
+  color: white;
+  min-width: 200px;
+  border-radius: 50px;
+}
+.my-btn:hover {
+  background-color: #338395;
+  color: #fde74c;
+}
+</style>
