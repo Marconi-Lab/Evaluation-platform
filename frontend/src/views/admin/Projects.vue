328166ea74cae85d9164b59be297b6fcd83495ca
ben-wycliff <ben12wycliff@gmail.com>
2022-10-15 09:47:25 +0300

    Initialize admin's projects page

---
+++ frontend/src/views/admin/Projects.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <UserNavigation :username="'Admin'" />
+  </div>
+</template>
+
+<script>
+import UserNavigation from "@/components/common/UserNavigation.vue";
+export default {
+  name: "admin-projects",
+  components: {
+    UserNavigation,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
