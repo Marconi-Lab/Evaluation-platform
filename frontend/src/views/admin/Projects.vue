d0a0b52f3206e7f7023b329d4876af8804610503
ben-wycliff <ben12wycliff@gmail.com>
2022-10-15 11:55:20 +0300

    Adds project addition button

--- frontend/src/views/admin/Projects.vue
+++ frontend/src/views/admin/Projects.vue
@@ -5,7 +5,13 @@
       <div class="my-4">
         <h4 class="text-center">The Marconi Lab ML Evaluation Platform</h4>
         <SearchBar />
-        <ProjectsTable />
+        <div class="m-5">
+          <div class="mb-3">
+            <PrimaryButton @onClick="handleAddProject" />
+          </div>
+
+          <ProjectsTable />
+        </div>
       </div>
     </div>
   </div>
@@ -15,12 +21,14 @@
 import UserNavigation from "@/components/common/UserNavigation.vue";
 import SearchBar from "@/components/common/SearchBar.vue";
 import ProjectsTable from "@/components/admin/ProjectsTable.vue";
+import PrimaryButton from "@/components/common/PrimaryButton.vue";
 export default {
   name: "admin-projects",
   components: {
     UserNavigation,
     SearchBar,
     ProjectsTable,
+    PrimaryButton,
   },
   data() {
     return {};
