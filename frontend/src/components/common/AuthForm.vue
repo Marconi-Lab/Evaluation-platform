9cfa84d245125280fe99e036490dc1fc0fcd0714
NakacwaOlivia <k.nakacwa.olivia@gmail.com>
2022-10-17 18:00:15 +0300

    changed the link path

--- frontend/src/components/common/AuthForm.vue
+++ frontend/src/components/common/AuthForm.vue
@@ -27,7 +27,7 @@
         id="exampleInputPassword1"
       />
     </div>
-    <router-link to="/user/projects" class="text-white"
+    <router-link to="projects" class="text-white"
       ><button
         type="submit"
         class="btn btn-primary my-4"
