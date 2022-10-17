7f787320e3f5785d21cb91252b8109147544f837
NakacwaOlivia <k.nakacwa.olivia@gmail.com>
2022-10-17 18:02:02 +0300

    changed link path

--- frontend/src/components/common/LandingNavigation.vue
+++ frontend/src/components/common/LandingNavigation.vue
@@ -1,8 +1,10 @@
 <template>
   <nav class="navbar navbar-expand-sm text-right">
     <div class="nav-list d-flex justify-content-end">
-      <router-link class="px-5 navlink" to="/login">Login</router-link>
-      <router-link class="px-5 mr-5 navlink" to="/signup">Register</router-link>
+      <router-link class="px-5 navlink" to="/user">Login</router-link>
+      <router-link class="px-5 mr-5 navlink" to="/user/signup"
+        >Register</router-link
+      >
     </div>
   </nav>
 </template>
