69036be142c773caaaf1aa3103e3bd9994270c51
NakacwaOlivia <k.nakacwa.olivia@gmail.com>
2022-10-19 17:13:44 +0300

    Getting score from db

---
+++ frontend/src/services/score.js
@@ -0,0 +1,9 @@
+module.export = [
+  {
+    id: 1,
+    image_url:
+      "https://d3i71xaburhd42.cloudfront.net/1afe2e2397e3ff74e488b52f693519f669332cb2/34-Figure2.3-1.png",
+    title: "Detecting diseases in the cassava crop",
+  },
+  {},
+];
